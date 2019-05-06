65f9a3abb2345179dba5482774d823736b9f1b31
Usuario <Usuario@AULA7PC17>
2019-05-06 11:17:05 -0300

    Nueva Clase "Pila"

---
+++ Practico4/Practico4/Pila.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico4
+{
+    class Pila
+    {
+        private int gei = 0;
+
+        public Pila (int gei)
+        {
+            gei = 210;
+        }
+    }
+}

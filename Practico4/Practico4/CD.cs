84d77a09b2ced492bb5ee0e57073ff37aa8b226a
Juanma <Juanma@LAPTOP-G0MR2NPJ>
2019-05-06 10:40:06 -0300

    puto

---
+++ Practico4/Practico4/CD.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practico4
+{
+    private int puto = 10;
+    class CD
+    {
+        public CD ()
+        {
+            puto = 20;
+        }
+    }
+}

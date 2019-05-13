af21c44edb6513a1004a6a98732ebc91f4a5eafd
Diego Pincheira <diego.m.p@live.com.ar>
2019-05-13 02:09:29 -0300

    testpila pasa los discos de una pila a otra pila

--- Practico4/Practico4/testpila.cs
+++ Practico4/Practico4/testpila.cs
@@ -10,16 +10,33 @@ namespace Practico4
     {
         static void Main(string[] args)
         {
-             string[] pila1 = new string[5] { "disco1", "disco2", "disco3", "disco4", "disco5" };
-             string[] pila2 = new string[5];
+            string[] pila1 = new string[5] { "disco1", "disco2", "disco3", "disco4", "disco5" };
+            string[] pila2 = new string[5];
 
+            Console.WriteLine("");
 
-             pila1.CopyTo(pila2, 0);
-             for (int i = 0; i<pila2.Length; i++)
-             {
-                Console.WriteLine("discos:"+pila2[i]);
-             }
-             Console.ReadKey();
+            for (int i = 0; i < pila1.Length; i++)
+            {
+                Console.WriteLine("discos de pila 1:" + pila1[i]);
+            }
+
+            Console.WriteLine("");
+
+            pila1.CopyTo(pila2, 0);
+            for (int i = 0; i < pila2.Length; i++)
+            {
+                Console.WriteLine("discos de pila 2:" + pila2[i]);
+                pila1[i] = null;
+            }
+
+            Console.WriteLine("");
+
+            for (int i = 0; i < pila1.Length; i++)
+            {
+                Console.WriteLine("discos de pila 1:" + pila1[i]);
+            }
+          
+            Console.ReadKey();
         }
     }
 }
